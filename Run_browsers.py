72475d2f4be4468ce987241f385bc550322e5f08
Daniil Ilinykh <daniilfirst@Daniils-MacBook-Air.local>
2022-11-28 19:25:50 +0300

    add new file run_browsers

---
+++ Run_browsers.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+#Develop -> Allow Remote Automation -> YES
+driver = webdriver.Safari()
+driver.get('https://cezarkazan.ru')
+
+#Need only geckodriver in path
+driver = webdriver.Firefox()
+driver.get('https://cezarkazan.ru')
+
+#Need only chromedriwer in path
+driver = webdriver.Chrome()
+driver.get('https://cezarkazan.ru')
+
