0fdaa7e0c1e0b491f901d5f0448499283c6df57c
Daniil Ilinykh <daniilfirst@Daniils-MacBook-Air.local>
2022-12-21 22:48:44 +0300

    add Task_13.py

--- Task_13.py
+++ Task_13.py
@@ -7,4 +7,38 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(3)
 
 
-driver.get('http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones')
+driver.get('http://localhost/litecart/en/')
+
+
+def buy_product(locator1, locator2):
+    driver.find_element(By.CSS_SELECTOR, f'{locator1}').click()
+    driver.find_element(By.CSS_SELECTOR, f'{locator2}').click()
+    time.sleep(2)
+    driver.back()
+    time.sleep(2)
+    check_count = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text
+    return check_count
+
+
+while int(buy_product('#box-most-popular > div > ul > li:nth-child(1) > a.link', 'button[name=add_cart_product]'))<3:
+    buy_product('#box-most-popular > div > ul > li:nth-child(1) > a.link', 'button[name=add_cart_product]')
+    
+
+
+def delete_product(locator1):
+    driver.find_element(By.CSS_SELECTOR, '#cart a.link').click()
+    driver.find_element(By.CSS_SELECTOR, f'{locator1}').click()
+    time.sleep(2)
+    driver.back()
+    time.sleep(2)
+    check_count = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text
+    return check_count
+
+
+while int(delete_product('button[name=remove_cart_item]')) != 0:
+    delete_product('button[name=remove_cart_item]')
+
+
+
+
+
