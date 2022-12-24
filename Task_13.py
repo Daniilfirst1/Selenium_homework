e423500f14e4949b21dd3ae686f78905e1988c51
Daniil Ilinykh <daniilfirst@Daniils-MacBook-Air.local>
2022-12-24 19:23:11 +0300

    change Task_7.py

--- Task_13.py
+++ Task_13.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 driver = webdriver.Chrome()
@@ -13,7 +14,9 @@ check_count = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.qua
 while int(check_count) < 3:
     driver.find_element(By.CSS_SELECTOR, '#box-most-popular > div > ul > li:nth-child(1) > a.link').click()
     driver.find_element(By.CSS_SELECTOR, 'button[name=add_cart_product]').click()
-    time.sleep(1)
+    check_count_wait = WebDriverWait(driver, timeout = 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[class=quantity]"))).text
+    print(check_count)
+    print(check_count_wait)
     driver.back()
     check_count = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text
 
@@ -23,7 +26,6 @@ check_count_ofter_delete = driver.find_element(By.CSS_SELECTOR, '#cart > a.conte
 while int(check_count_ofter_delete) != 0:
     driver.find_element(By.CSS_SELECTOR, '#cart a.link').click()
     driver.find_element(By.CSS_SELECTOR, 'button[name=remove_cart_item]').click()
-    time.sleep(1)
     driver.back()
     check_count_ofter_delete = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text
 
--- Task_7.py
+++ Task_7.py
@@ -9,18 +9,8 @@ driver.implicitly_wait(3)
 
 driver.get('http://localhost/litecart/en/')
 
-most_popular = driver.find_element(By.ID, 'box-most-popular')
-stikers_in_mos_popular = most_popular.find_elements(By.CSS_SELECTOR, 'div li:nth-child(2)')
+stikers_in_mos_popular = driver.find_elements(By.CSS_SELECTOR, 'li.product.column.shadow.hover-light')
 for x in stikers_in_mos_popular:
     assert len(x.find_elements(By.CSS_SELECTOR, 'div.sticker')) == 1
 
-box_compaings = driver.find_element(By.ID, 'box-campaigns')
-stikers_in_box_compaings= most_popular.find_elements(By.CSS_SELECTOR, 'div li')
-for x in stikers_in_box_compaings:
-    assert len(x.find_elements(By.CSS_SELECTOR, 'div.sticker')) == 1
-
-box_latest_products = driver.find_element(By.ID, 'box-latest-products')
-stikers_in_box_latest_products= most_popular.find_elements(By.CSS_SELECTOR, 'div > ul > li:nth-child(1)')
-for x in stikers_in_box_latest_products:
-    assert len(x.find_elements(By.CSS_SELECTOR, 'div.sticker')) == 1
 
