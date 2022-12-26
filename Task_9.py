5147be48e4c021a0a1e98edb20c309facc6eae6e
Daniil Ilinykh <daniilfirst@Daniils-MacBook-Air.local>
2022-12-26 21:04:16 +0300

    change Task_9.py

--- Task_9.py
+++ Task_9.py
@@ -23,28 +23,17 @@ inter_button.click()
 list_geozones = []
 x = [2, 3]
 
-# for i in x:
-#     countries = driver.find_element(By.XPATH, f'//*[@id="content"]/form/table/tbody/tr[{i}]/td[3]')
-#     link = countries.find_element(By.CSS_SELECTOR, 'a').click()
-#     time.sleep(1)
-#     a = Select(driver.find_elements(By.CSS_SELECTOR, 'select'))
-#     for d in a.options:
-#         print(d.text)
 
 for i in x:
     countries = driver.find_element(By.XPATH, f'//*[@id="content"]/form/table/tbody/tr[{i}]/td[3]')
     link = countries.find_element(By.CSS_SELECTOR, 'a').click()
     time.sleep(1)
-    count_zones = driver.find_elements(By.CSS_SELECTOR, 'select.select2-hidden-accessible')
-    len_zones = len(count_zones)+2
-    for x in range(len_zones)[2:]:
-        zones = driver.find_element(By.XPATH, f'//*[@id="table-zones"]/tbody/tr[{x}]/td[3]/select').get_attribute('selected')
-        list_geozones.append(zones)
-    assert list_geozones == sorted(list_geozones)
+    a = driver.find_elements(By.CSS_SELECTOR, 'tr td:nth-child(3) > select option[selected]')
+    for x in a:
+        list_geozones.append(x.text)
+        assert list_geozones == sorted(list_geozones)
     list_geozones.clear()
     driver.back()
-    
-
 
     
     
