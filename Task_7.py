c8ee1e7213c59f32158fa6f4799fef22f60d8402
Daniil Ilinykh <daniilfirst@Daniils-MacBook-Air.local>
2022-12-26 19:33:04 +0300

    change locator Task_7.py

--- Task_7.py
+++ Task_7.py
@@ -9,7 +9,7 @@ driver.implicitly_wait(3)
 
 driver.get('http://localhost/litecart/en/')
 
-stikers_in_mos_popular = driver.find_elements(By.CSS_SELECTOR, 'li.product.column.shadow.hover-light')
+stikers_in_mos_popular = driver.find_elements(By.CSS_SELECTOR, 'li.product')
 for x in stikers_in_mos_popular:
     assert len(x.find_elements(By.CSS_SELECTOR, 'div.sticker')) == 1
 
