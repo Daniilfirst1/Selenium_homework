ba03181b677e7be9d95d0bbba39083543c293acf
Daniil Ilinykh <daniilfirst@Daniils-MacBook-Air.local>
2022-12-27 20:59:10 +0300

    /usr/local/bin/python3 /Users/daniilfirst/Selenium_homework/Task_13.py

--- Task_13.py
+++ Task_13.py
@@ -1,12 +1,14 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(3)
-
+wait = WebDriverWait(driver, 3)
 
 driver.get('http://localhost/litecart/en/')
 
@@ -14,9 +16,15 @@ check_count = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.qua
 while int(check_count) < 3:
     driver.find_element(By.CSS_SELECTOR, '#box-most-popular > div > ul > li:nth-child(1) > a.link').click()
     driver.find_element(By.CSS_SELECTOR, 'button[name=add_cart_product]').click()
-    check_count_wait = WebDriverWait(driver, timeout = 10).until(EC.any_of(By.CSS_SELECTOR, "span[class=quantity]"))
-    driver.back()
-    check_count = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text
+    try:
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[name=add_cart_product][style=""')))
+    except Exception:
+        Select(driver.find_element(By.CSS_SELECTOR, 'select[name="options[Size]"]')).select_by_value('Small')
+        driver.find_element(By.CSS_SELECTOR, 'button[name=add_cart_product]').click()
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[name=add_cart_product][style=""')))
+    finally:
+        driver.back()
+        check_count = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text
 
 
 
@@ -24,6 +32,7 @@ check_count_ofter_delete = driver.find_element(By.CSS_SELECTOR, '#cart > a.conte
 while int(check_count_ofter_delete) != 0:
     driver.find_element(By.CSS_SELECTOR, '#cart a.link').click()
     driver.find_element(By.CSS_SELECTOR, 'button[name=remove_cart_item]').click()
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div [id=checkout-cart-wrapper][style="opacity: 1;"]')))
     driver.back()
     check_count_ofter_delete = driver.find_element(By.CSS_SELECTOR, '#cart > a.content > span.quantity').text
 
